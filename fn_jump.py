8ac11699e86dabe16e5f6a17d78bb83202af7cca
Matix <116652735+MatixGX@users.noreply.github.com>
2024-11-06 22:10:06 +0100

    Create files

---
+++ fn_jump.py
@@ -0,0 +1,13 @@
+import pyautogui as pag
+import keyboard
+import time
+
+pag.PAUSE = 0.00
+
+count = 100
+
+while True:
+    if keyboard.is_pressed(";"):
+        print("Ultra jump!!!")
+        for i in range(count):
+            pag.press("/")
